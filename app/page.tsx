ef3b996c64ce90178bb9e86ea26aeab71d15431b
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-05-31 17:10:46 +0900

    fix: package setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,6 +9,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <Link href="/">
+          <h1>aaaaaaaaaaaaa</h1>
+        </Link>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
