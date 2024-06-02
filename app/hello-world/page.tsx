a9dd47813b6927202dd49d0717f39f436b07fa33
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-06-02 10:39:28 +0900

    fix: rename Component name and change rendering to serverside on /hello-world

--- app/hello-world/page.tsx
+++ app/hello-world/page.tsx
@@ -1,4 +1,5 @@
-export default function Home() {
+'use server'
+export default async function HelloWorld() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <h1 className="">hello-world</h1>
