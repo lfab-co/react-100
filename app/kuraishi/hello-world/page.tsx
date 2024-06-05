3d6dd74aeeb3d4995f63256dc558647015989315
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-06-05 15:28:24 +0900

    feat: add counter to /kuraishi

---
+++ app/kuraishi/hello-world/page.tsx
@@ -0,0 +1,8 @@
+'use server'
+export default async function HelloWorld() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="">hello-world</h1>
+    </div>
+  )
+}
