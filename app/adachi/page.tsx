f7b6c66afa8c8a1ba8ba2b77d44f5424f6638b52
Kuraishi Taiki <131664944+taiki-kuraishi@users.noreply.github.com>
2024-06-05 12:29:18 +0900

    fix: dir-structure (#3)

---
+++ app/adachi/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Adachi() {
+  return (
+    <div>
+      <Link href="/adachi/hello-world">hello-world 001</Link>
+    </div>
+  )
+}
---
+++ app/kajiwara/page.tsx
@@ -0,0 +1,3 @@
+export default function Kajiwara() {
+  return <div>kajiwara pege</div>
+}
