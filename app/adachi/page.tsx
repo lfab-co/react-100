cc70250e6a507e21e352e3728f24e91ce5c68859
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-06-06 17:22:47 +0900

    fix: conflict by merge main

--- app/adachi/page.tsx
+++ app/adachi/page.tsx
@@ -4,6 +4,7 @@ export default function Adachi() {
   return (
     <div>
       <Link href="/adachi/hello-world">hello-world 001</Link>
+      <Link href="/adachi/counter">hello-world 001</Link>
     </div>
   )
 }
