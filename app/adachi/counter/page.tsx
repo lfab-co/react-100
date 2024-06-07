b5f9422c8d53e859c5d7398a5b6e01db4b55cc0c
0krm3 <t03rme@gmail.com>
2024-06-07 11:43:48 +0900

    Change counter CSS

--- app/adachi/counter/page.tsx
+++ app/adachi/counter/page.tsx
@@ -5,11 +5,23 @@ const Counter: React.FC = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className="text-center">React Counter</h1>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div>
+        <h1 className="text-center text-4xl">React Counter</h1>
+        <h2 className="m-10 text-center text-6xl">{count}</h2>
+        <button
+          onClick={() => setCount(count + 1)}
+          className="m-3 size-24 rounded-full bg-red-200 text-3xl"
+        >
+          +
+        </button>
+        <button
+          onClick={() => setCount(count - 1)}
+          className="m-3 size-24 rounded-full bg-red-50 text-3xl"
+        >
+          -
+        </button>
+      </div>
     </div>
   )
 }
