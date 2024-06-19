7cf98161a2004db20b9f200a260304677365a6c3
Rinshanone <116053741+Rinshanone@users.noreply.github.com>
2024-06-19 11:23:46 +0900

    counter用のページを作成

--- app/kajiwara/page.tsx
+++ app/kajiwara/page.tsx
@@ -4,6 +4,8 @@ export default function Kajiwara() {
   return (
     <div>
       <Link href="/kajiwara/hello-world">001 Hello World</Link>
+      <br />
+      <Link href="/kajiwara/counter">002 counter</Link>
     </div>
   )
 }
